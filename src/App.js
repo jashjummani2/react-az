99b1da18374bbbd5299329cd217583dcd179401a
JashJummani <jash.logicwind@gmail.com>
2026-08-16 00:46:36 +0530

    basic react app setup done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <h1>Hi This is my first react app!!</h1>
+  );
+}
+// JSX is the javascript extention where you can return HTML objects
+// it should start with capital word
+
+export default App;
