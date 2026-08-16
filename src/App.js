d221f3b2911f3e274d2deb556115b6a7c85c3232
JashJummani <jash.logicwind@gmail.com>
2026-08-16 16:40:57 +0530

    Usage of props and deconstucting the variable use in react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Navbar from "./components/navbar";
+const getRandomNumber = ()=>{
+  return Math.floor(Math.random()*100);
+}
+const showDesc = false;
 
+const isloggedProps = true;
 function App() {
   return (
-    <h1>Hi This is my first react app!!</h1>
+    <div>
+      <Navbar isloggedProps= {isloggedProps}/>
+      <h1>Hi This is my first react app!!</h1>
+      {showDesc ? <p1>{getRandomNumber()}</p1> : <p1>No Description</p1>}
+    </div>
   );
 }
 // JSX is the javascript extention where you can return HTML objects
