5acca7f0005580c3b77d2b879dbc8439bfdb25f8
JashJummani <jash.logicwind@gmail.com>
2026-08-22 09:22:36 +0530

    passing variable names in handleclick function. on handle click we cant use onClick() with parathensis, it means we're calling it, to let the react call it use onClick only

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -3,7 +3,7 @@ import './navbar.css';
 function NavItem({onNavItemClick, children}){
   
   return (
-    <li className="nav-item" onClick={onNavItemClick}>
+    <li className="nav-item" onClick={() =>onNavItemClick(children)}>
       <div className="nav-item-link">{children}</div>
     </li>
   );
@@ -12,8 +12,8 @@ function NavItem({onNavItemClick, children}){
 function Navbar(props) {
     // or do Navbar({isloggedProps}) de-constucting the data variable
     // console.log(props);
-    function handleClick(){
-    console.log("HELLO WORLD");
+    function handleClick(text){
+    console.log("HELLO WORLD"+text);
     // console.log(props.children);
   }
   return (
@@ -22,8 +22,8 @@ function Navbar(props) {
       <p>{props.isloggedProps ? "Logged In" : "Logged Out"}</p>
       <ul className="navbar">
         <NavItem onNavItemClick ={handleClick}>About</NavItem>
-        <NavItem>Contact</NavItem>
-        <NavItem>Projects</NavItem>
+        <NavItem onNavItemClick ={handleClick}>Contact</NavItem>
+        <NavItem onNavItemClick ={handleClick}>Projects</NavItem>
         {/* <NavItem text="About"/>
         <NavItem text="Contact"/> */}
       </ul>
