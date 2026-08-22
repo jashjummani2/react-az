c47e1431056646b886480d7c00d927120b797187
JashJummani <jash.logicwind@gmail.com>
2026-08-22 09:16:59 +0530

    handling onClick functionality just for one component

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,22 +1,29 @@
 import './navbar.css';
 
-function NavItem(props){
+function NavItem({onNavItemClick, children}){
+  
   return (
-    <li className="nav-item">
-      <div className="nav-item-link">{props.children}</div>
+    <li className="nav-item" onClick={onNavItemClick}>
+      <div className="nav-item-link">{children}</div>
     </li>
   );
 }
 
 function Navbar(props) {
     // or do Navbar({isloggedProps}) de-constucting the data variable
-    console.log(props);
+    // console.log(props);
+    function handleClick(){
+    console.log("HELLO WORLD");
+    // console.log(props.children);
+  }
   return (
     <div className="navbar-wrapper">
   
       <p>{props.isloggedProps ? "Logged In" : "Logged Out"}</p>
       <ul className="navbar">
-        <NavItem>About</NavItem>
+        <NavItem onNavItemClick ={handleClick}>About</NavItem>
+        <NavItem>Contact</NavItem>
+        <NavItem>Projects</NavItem>
         {/* <NavItem text="About"/>
         <NavItem text="Contact"/> */}
       </ul>
